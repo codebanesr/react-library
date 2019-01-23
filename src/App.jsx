af6a4bfeec3ec40a2ef8e1b11ee8ca8ed30d6833
shanurrahman <rshanur@deepiq.com>
2019-01-24 02:40:12 +0530

    react setup
    simple login and homepage

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react'
+import HomePage from './components/HomePage';
+import {Route} from 'react-router-dom'
+import LoginPage from './components/LoginPage';
+
+export default class App extends PureComponent {
+  render() {
+    return (
+      <div>
+        <Route path='/' exact component={HomePage}/>
+        <Route path='/login' component={LoginPage}/>
+      </div>
+    )
+  }
+}
