e3f81da1b3a2d6eb0801eb98d2d4c7a7570f457d
shanurrahman <rshanur@deepiq.com>
2019-01-26 12:49:05 +0530

    added devtools, redux and wrote a root reducer function
    basic scaffolding of a react-redux application

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,14 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './rootReducer';
+import { composeWithDevTools } from 'redux-devtools-extension'
 
+
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 ReactDOM.render(
     <BrowserRouter>
         <App />
